d9f726f91adc48a9e36cd9a6b8c839712fb989c7
Nafiz Iqbal <f.nafiziqbal@gmail.com>
2023-08-13 18:14:18 +0600

    INit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import MainLayout from "./components/Shared/Layout/MainLayout";
+
+
+function App() {
+  return (
+    <div>
+      <MainLayout />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+
+const Home = () => {
+  return (
+    <div>
+      <p>This Is Home Route</p>
+    </div>
+  );
+};
+
+export default Home;
